1be8e2b4ca6216ffb5098aa269556c6f23d67cda
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-10 16:40:22 +0530

    DataTypes

--- Hello.java
+++ Hello.java
@@ -1,11 +1,23 @@
 public class Hello {
     public static void main(String [] args) {
+        double marks = 6.7;
+        float score = 1.89f;
         int num = 10; // "= is a assignment operator"
-        int num2 = 79;
+        int num2 = 79;  
+        char s = 'k';
+        String name = "Sayali Rajwade";
+        boolean answer = true;
+        long phone_number = 9021628100l;
        System.out.println(num+5); 
        System.out.println(10/2); 
        System.out.println(9-num2); 
        System.out.println(num*5); 
+       System.out.println(marks);
+       System.out.println(score);
+       System.out.println(name);
+       System.out.println(s);
+       System.out.println(answer);
+       System.out.println(phone_number);
 
     }
     
