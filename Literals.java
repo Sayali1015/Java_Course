be0ce4e74e7818eadbf92cbbe6853417cc85ad6a
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-10 16:49:07 +0530

    Literals

---
+++ Literals.java
@@ -0,0 +1,18 @@
+public class Literals {
+
+    public static void main(String[] args) {
+        int num = 100;
+        int num1= 190;
+        int result  = num * num1; 
+        System.out.println(result);
+
+        char c = 'a';
+        c++;
+        System.out.println(c);
+
+        char v = 'b';
+        v--;
+        System.out.println(v);
+
+    }
+}
