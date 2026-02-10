81bebf31ba67bf904d10112eda4367561ccdefcb
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-10 16:26:42 +0530

    Printing Statment and variable

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(String [] args) {
-       System.out.println("Hello World!"); 
+        int num = 10; // "= is a assignment operator"
+        int num2 = 79;
+       System.out.println(num+5); 
+       System.out.println(10/2); 
+       System.out.println(9-num2); 
+       System.out.println(num*5); 
+
     }
     
 }
