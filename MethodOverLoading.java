4855d7e7c4190d8cd5c76fd3390d9c0d119c33ba
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-11 17:21:22 +0530

    More On Head Area , Global or Instance Variable

--- MethodOverLoading.java
+++ MethodOverLoading.java
@@ -1,5 +1,6 @@
 //If we have same method name with different parameters the this is call as method overloading
 class Calculator {
+    int num = 5; // this is a global or a instance variable which will get place inside heap area.
     public int add (int n1 , int n2, int n3){
         return n1 + n2 + n3;
     }
@@ -16,9 +17,16 @@ class Calculator {
 public class MethodOverLoading {
     public static void main(String[] args) {
         Calculator obj = new Calculator();
-        System.out.println(obj.add(10, 100, 80));
-        System.out.println(obj.add(89, 45));
-        System.out.println(obj.add(78.88, 82));
+        Calculator obj1 = new Calculator();
+
+        obj.num = 9;
+
+        // System.out.println(obj.add(10, 100, 80));
+        // System.out.println(obj.add(89, 45));
+        // System.out.println(obj.add(78.88, 82));
+
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
 
     }
 }
