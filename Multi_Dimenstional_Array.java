18ceec14029d0df5169764cb1a61d70c538fa97e
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-11 21:06:33 +0530

    MultiDimenstional Array using random() to generate random values

---
+++ Multi_Dimenstional_Array.java
@@ -0,0 +1,20 @@
+public class Multi_Dimenstional_Array {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4]; // 3rows and 4columns
+
+         for(int i =0; i<3; i++){
+
+            for(int j=0; j<4; j++){
+               nums [i][j] = (int)(Math.random() * 10); // here Math.random() is used to generate random values and random() give decimal values thats why to get whole number we have multiplied it by 10(for single number) or 100(for 2gigit numbers), also did type casting double to int
+            }
+        }
+    
+
+        for(int i =0; i<3; i++){
+            for(int j=0; j<4; j++){
+                System.out.print(nums[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
