9e1f9c98204926a3e2bc5a60333b5c19ae26d480
Sayali <165777188+Sayali1015@users.noreply.github.com>
2026-02-19 14:46:01 +0530

    Implement upcasting and downcasting examples in Java
    Added classes A and B to demonstrate upcasting and downcasting in Java. (TypeCasting in Oops Concept)

--- Down_UpCasting.java
+++ Down_UpCasting.java
@@ -1,3 +1,17 @@
+class A{
+
+    public void show1(){
+        System.out.println("in A show()");
+    }
+}
+
+class B extends A{
+
+    public void show2(){
+        System.out.println("in B show()");
+    }
+}
+
 public class Down_UpCasting {
     public static void main(String[] args) {
         
@@ -5,5 +19,12 @@ public class Down_UpCasting {
         double a = 10.5;
         int b = (int) a; // this is called as downcasting because we are converting a higher data type to lower data type. This is also called as explicit type casting because we are explicitly mentioning the data type to which we want to convert.
         System.out.println(b);
+
+
+        A obj = new B(); // upcasting because we are converting a lower data type to higher data type. This is also called as implicit type casting because we are not explicitly mentioning the data type to which we want to convert.
+        obj.show1(); 
+
+        B obj1 = (B) obj; // downcasting because we are converting a higher data type to lower data type. This is also called as explicit type casting because we are explicitly mentioning the data type to which we want to convert.
+        obj1.show2();   
     }
 }
