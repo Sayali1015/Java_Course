f25bd0c09a617fe78f4d3b139e34b94a480994dc
Sayali <165777188+Sayali1015@users.noreply.github.com>
2026-02-21 15:23:16 +0530

    Modify functional interface to accept two parameters
    added the lambda expression in the example

--- functionalinterface.java
+++ functionalinterface.java
@@ -1,6 +1,6 @@
 @FunctionalInterface
 interface A{
-    void show();
+    void show( int i, int j);
 }
 
 // class B implements A{
@@ -11,15 +11,12 @@ interface A{
 
 public class functionalinterface {
     public static void main(String[] args) {
-        A obj = new A(){
-            public void show(){
-                System.out.println("This is class B");
-            }
-        };
-        obj.show();
+        A obj = (int i , int j) ->  System.out.println("in show " + i + " " + j); // if here i have only 1 variable then there is no need of mentioning () brackets and no datatype of the varibale ex : A obj = i -> System.out.println("in show " + i);
+        obj.show(5, 10);
     }
 }
 
 
 //In java 8 we got an amazaing feature called lambda expression.
 //Lambda expression can be used only with functional interfaces.
+//Lambda expression is a short form of anonymous class. By using -> operator we can create a lambda expression.
