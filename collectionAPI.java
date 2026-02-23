07546e4db2959ba86d8cde56106e8b6d9e3fc9c8
Sayali <165777188+Sayali1015@users.noreply.github.com>
2026-02-23 16:31:12 +0530

    Update collectionAPI.java

--- collectionAPI.java
+++ collectionAPI.java
@@ -6,19 +6,51 @@
 //types of collection List, Set, Queue, Map
 //<type> is called as a generic type. It is used to specify the type of elements that a collection can hold. For example, Collection<Integer> means that the collection can only hold Integer objects. This helps to ensure type safety and prevents runtime errors when working with collections.
 
-import java.util.ArrayList;
-import java.util.Collection;
+//Collection works with objects and it does not work with primitive data types. To use primitive data types with collections, we can use wrapper classes like Integer for int, Double for double, etc.<Integer> is a wrapper class for int.
+//List works with index ans it allows duplicate values.
+//Set does not work with index and it does not allow duplicate values. hashset is a class that implements set interface.it does not maintain any order of elements. 
+//Map wotks with key and value pairs. It does not allow duplicate keys but it allows duplicate values. HashMap is a class that implements Map interface. It does not maintain any order of elements.
+
+
+//Differnec between HashMap and HashTable
+
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.HashSet;
+// import java.util.List;
+// import java.util.Set;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class collectionAPI {
     public static void main(String[] args) {
-        Collection<Integer> nums = new ArrayList<Integer>();
-        nums.add(6);
-        nums.add(9);
-        nums.add(4);
+        // Set<Integer> nums = new HashSet<Integer>();
+        // nums.add(6);
+        // nums.add(9);
+        // nums.add(4);
+        // nums.add(6);
 
-        for(int n : nums){
-            System.out.println(n);
+      
+        // for(int n : nums){
+        //     System.out.println(n);
+        // }
+
+
+        Map<String, Integer> students = new HashMap<>();
+        students.put("Sayali",  89);
+        students.put( "Durvesh",  90);
+        students.put( "Kunal",  98);
+        students.put( "Diksha",  95);
+        students.put("Sayali", 99);
+
+        System.out.println(students);
+        System.out.println(students.get("Sayali"));
+        System.out.println(students.keySet());
+
+        for(String name : students.keySet()){
+            System.out.println(name + " : "+ students.get(name));
         }
-        
+
     }
 }
