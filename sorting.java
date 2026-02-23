d9bc407b97b564090256140db361767498431179
Sayali <165777188+Sayali1015@users.noreply.github.com>
2026-02-23 16:57:39 +0530

    Create sorting.java

---
+++ sorting.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+class Student{
+    int age;
+    String name;
+
+    
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+}
+
+
+public class sorting {
+    public static void main(String[] args) {
+
+    Comparator<Student> com = new Comparator<Student>()
+     {
+        public int compare(Student i, Student j){
+            if (i.age > j.age)
+                return 1;
+            else
+                return -1;
+        }
+    };
+   
+
+        List<Student> studs = new ArrayList<>();
+        studs.add(new Student(21, "Sayali"));
+        studs.add(new Student(18, "Om"));
+        studs.add(new Student(20, "Kunal"));
+        studs.add(new Student(26, "Durvesh"));
+
+        Collections.sort(studs, com);
+        for (Student s: studs)
+            System.out.println(s);
+
+    
+}
+}
